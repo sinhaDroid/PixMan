293c01c822f9489eaed0fe04a7878e698f4af18d
Deepanshu Sinha <107sinha@gmail.com>
2020-01-24 22:29:06 +0530

    onactivityresult handled in fragment, bugs fixes

--- app/src/main/java/com/example/pixman/ui/MainActivity.kt
+++ app/src/main/java/com/example/pixman/ui/MainActivity.kt
@@ -1,27 +1,17 @@
 package com.example.pixman.ui
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.pixman.BR
 import com.example.pixman.R
 import com.example.pixman.databinding.ActivityMainBinding
 import com.example.pixman.ui.base.BaseActivity
 import com.example.pixman.ui.view.EditImageFragment
-import com.example.pixman.ui.view.HomeFragment
 import com.example.pixman.ui.viewmodel.MainViewModel
-import com.example.pixman.util.Constants.RequestKeys.Companion.CAMERA_REQUEST
-import com.example.pixman.util.Constants.RequestKeys.Companion.PICK_REQUEST
 import com.example.pixman.util.getFragmentInstance
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.IOException
 
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
@@ -42,11 +32,11 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
         // Set up toolbar with navigation
 //        setupActionBarWithNavController(navController, AppBarConfiguration(setOf(R.id.home)))
 
-        navController.addOnDestinationChangedListener { _, destination, _ ->
+        /*navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.editImage -> makeFullScreen()
             }
-        }
+        }*/
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -62,36 +52,6 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            when (requestCode) {
-                CAMERA_REQUEST -> {
-                    val photo = data?.extras!!["data"] as Bitmap
-                    getFragmentInstance<HomeFragment>(
-                        supportFragmentManager,
-                        R.id.nav_host_fragment_main
-                    ) {
-                        navigateToEdit(photo)
-                    }
-                }
-                PICK_REQUEST -> try {
-                    val uri = data?.data
-                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
-                    getFragmentInstance<HomeFragment>(
-                        supportFragmentManager,
-                        R.id.nav_host_fragment_main
-                    ) {
-                        navigateToEdit(bitmap)
-                    }
-
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
         }
     }
 
--- app/src/main/java/com/example/pixman/ui/view/HomeFragment.kt
+++ app/src/main/java/com/example/pixman/ui/view/HomeFragment.kt
@@ -1,8 +1,10 @@
 package com.example.pixman.ui.view
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.example.pixman.BR
@@ -10,7 +12,10 @@ import com.example.pixman.R
 import com.example.pixman.databinding.FragmentHomeBinding
 import com.example.pixman.ui.base.BaseFragment
 import com.example.pixman.ui.viewmodel.MainViewModel
+import com.example.pixman.util.Constants.RequestKeys.Companion.CAMERA_REQUEST
 import com.example.pixman.util.Constants.RequestKeys.Companion.PICK_REQUEST
+import java.io.IOException
+
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, MainViewModel>() {
 
@@ -24,7 +29,10 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, MainViewModel>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        dataBinding.camera.setOnClickListener {  }
+        dataBinding.camera.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, CAMERA_REQUEST)
+        }
 
         dataBinding.gallery.setOnClickListener {
             val intent = Intent()
@@ -36,5 +44,25 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, MainViewModel>() {
 
     internal fun navigateToEdit(bitmap: Bitmap) {
         findNavController().navigate(HomeFragmentDirections.actionHomeToEditImage(bitmap))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                CAMERA_REQUEST -> {
+                    val photo = data?.extras!!["data"] as Bitmap
+                    navigateToEdit(photo)
+                }
+                PICK_REQUEST -> try {
+                    val uri = data?.data
+                    val bitmap =
+                        MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, uri)
+                    navigateToEdit(bitmap)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
